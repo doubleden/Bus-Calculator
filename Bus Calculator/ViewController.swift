6ffc4d51cfd41073a384a98dd921730cafdecb07
Denis Denisov <denis.denisov8701@gmail.com>
2024-01-31 21:33:52 +0100

    calc logic

--- Bus Calculator/ViewController.swift
+++ Bus Calculator/ViewController.swift
@@ -7,13 +7,39 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+final class ViewController: UIViewController {
 
+    @IBOutlet var startSumTextField: UITextField!
+    @IBOutlet var taxLabel: UILabel!
+    @IBOutlet var planLabel: UILabel!
+    @IBOutlet var calculateButtonLabel: UIButton!
+    
+    private let tax = 12.7 / 100.0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        startSumTextField.keyboardType = .numberPad
+        calculateButtonLabel.layer.cornerRadius = 15
+        
+        taxLabel.clipsToBounds = true
+        taxLabel.layer.cornerRadius = 10
+        planLabel.clipsToBounds = true
+        planLabel.layer.cornerRadius = 10
+        
+        let tapGesture = UITapGestureRecognizer(target: self, action: #selector(hideKeyboard))
+            view.addGestureRecognizer(tapGesture)
     }
-
-
+    
+    @objc func hideKeyboard() {
+        view.endEditing(true)
+    }
+    
+    @IBAction func calculateButtonDidTapped() {
+        if let startSum = Double(startSumTextField.text ?? "0") {
+            let sumAfterTax = Int(startSum - (startSum * tax))
+            taxLabel.text = String(sumAfterTax)
+            planLabel.text = String(sumAfterTax - 100000)
+        }
+    }
 }
 
