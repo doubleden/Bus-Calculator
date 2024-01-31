ff5bea74efb76c6e4b290dec3c29eeddd77c64db
Denis Denisov <denis.denisov8701@gmail.com>
2024-01-31 20:23:23 +0100

    Initial Commit

---
+++ Bus Calculator/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Bus Calculator
+//
+//  Created by Denis Denisov on 31/1/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
