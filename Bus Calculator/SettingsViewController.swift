b12266f4093f2948d20314931dee3b5d946be2f2
Denis Denisov <denis.denisov8701@gmail.com>
2024-02-10 18:01:22 +0100

    add SettingsVC

---
+++ Bus Calculator/SettingsViewController.swift
@@ -0,0 +1,16 @@
+//
+//  SettingsViewController.swift
+//  Bus Calculator
+//
+//  Created by Denis Denisov on 10/2/24.
+//
+
+import UIKit
+
+final class SettingsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+}
