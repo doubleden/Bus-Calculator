df3508e2b68a04c3d58dfb33127b5683c30d0e3e
Denis Denisov <denis.denisov8701@gmail.com>
2024-02-10 17:45:44 +0100

    change UI and added navigator bar

--- Bus Calculator/MainViewController.swift
+++ Bus Calculator/MainViewController.swift
@@ -7,7 +7,7 @@
 
 import UIKit
 
-final class ViewController: UIViewController {
+final class MainViewController: UIViewController {
 
     @IBOutlet var startSumTextField: UITextField!
     @IBOutlet var taxLabel: UILabel!
