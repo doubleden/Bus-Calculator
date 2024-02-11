de37129c8a3750b5c71470d1d00a6bf8cb036977
Denis Denisov <denis.denisov8701@gmail.com>
2024-02-11 14:53:24 +0100

    prepare settingsVC

--- Bus Calculator/SettingsViewController.swift
+++ Bus Calculator/SettingsViewController.swift
@@ -15,8 +15,25 @@ final class SettingsViewController: UIViewController {
     @IBOutlet var taxTextField: UITextField!
     @IBOutlet var planTextField: UITextField!
     
+    var currentTax: String!
+    var currentPlan: String!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        currentTaxValueLabel.text = "Текущий процент вычесления: \(currentTax ?? "")"
+        currentPlanValueLabel.text = "Текущий план: \(currentPlan ?? "")"
+        
+        taxTextField.addTarget(
+            self,
+            action: #selector(textFieldDidChange(_:)),
+            for: .editingChanged
+        )
+        
         taxTextField.becomeFirstResponder()
+    }
+    
+    @IBAction func cancelButtonTapped() {
+        dismiss(animated: true)
     }
 }
